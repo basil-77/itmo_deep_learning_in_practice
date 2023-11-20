94955a60bf5b481a0530c438a146e2a1bd608166
AshimIsha <krz.m@ya.ru>
2023-11-20 17:58:50 +0300

    no errors no youtube

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ Created on Sat Nov 18 16:18:56 2023
 """
 
 import streamlit as st
-import pytube as YT
-import os
 
+import os
 from streamlit_player import st_player
-from utils.model import load_model, local_video_processing
+from utils.model import SignDetection 
 
 SOURCE_LINK = None 
 VIDEO_PATH = 'videos'
 
-Test_model = load_model(f'{os.getcwd()}\\utils\\yolov8n.pt')
+TestModel = SignDetection(f'{os.getcwd()}\\utils\\best.pt') 
+
 
 def main():
     
@@ -27,39 +27,43 @@ def main():
     column1, column2 = st.columns([5,5])
     
     
-    
-
-    with st.sidebar:
-        
-        st.title("Выбор видео")
-        option = st.radio("Источник",
-                              ["Локально (.mp4)", "Youtube"],
-                              index=None)
-        
-        download_video=st.empty()
-        
-        if option == 'Локально (.mp4)':
-            download_video = st.sidebar.file_uploader(label="Выберите видео",\
-                                                      type='mp4',\
-                                                      accept_multiple_files=False)
-            with open(os.path.join(f'{VIDEO_PATH}', download_video.name),"wb") as f:
-                      f.write(download_video.getbuffer())
-                      
-        elif option == 'Youtube':
-            SOURCE_LINK = st.sidebar.text_input("ссылка на видео YouTube")
+    try:
+        with st.sidebar:
             
-    
-    with column1:
-        try:    
+            st.title("Выбор видео")
+            option = st.radio("Источник",
+                                  ["Локально (.mp4)", "Youtube"],
+                                  index=None)
+            
+            download_video=st.empty()
+            
+                
+                
+            if option == 'Локально (.mp4)':
+                download_video = st.sidebar.file_uploader(label="Выберите видео",\
+                                                          type='mp4',\
+                                                          accept_multiple_files=False)
+                with open(os.path.join(f'{VIDEO_PATH}', download_video.name),"wb") as f:
+                          f.write(download_video.getbuffer())
+                          
+            elif option == 'Youtube':
+                SOURCE_LINK = st.sidebar.text_input("ссылка на видео YouTube")
+                
+        
+        with column1:
+            
             if download_video:
-                local_video_processing(VIDEO_PATH, download_video, Test_model)
+                TestModel.local_video_processing(VIDEO_PATH, download_video)
+                #TestModel.key_points(VIDEO_PATH)
                 column2.write('')
-            if SOURCE_LINK:
+            elif SOURCE_LINK:
                 st_player(SOURCE_LINK)
-        except Exception as e:
-            st.sidebar.error("Видео не выбрано")
+                #TestModel.youtube_video_processing(SOURCE_LINK)
 
+    
 
+    except Exception as e:
+        st.sidebar.error("Видео не выбрано") 
 
     
 
